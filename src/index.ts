99cdfd9d34ed2ef4050b1175a45152a963adc136
nicolastestingthings <gabryelthomasantunes@gmail.com>
2026-08-09 16:30:49 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,47 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { after } from "@vendetta/patcher";
+import { findByProps } from "@vendetta/metro/common";
 
-export default {
-    onLoad: () => {
-        logger.log("Hello world!");
-    },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
-}
+const RADIUS_PROPS = new Set([
+    "borderRadius",
+    "borderTopLeftRadius",
+    "borderTopRightRadius",
+    "borderBottomLeftRadius",
+    "borderBottomRightRadius",
+    "borderTopStartRadius",
+    "borderTopEndRadius",
+    "borderBottomStartRadius",
+    "borderBottomEndRadius",
+]);
+
+let unpatch: (() => void) | null = null;
+
+export const onLoad = () => {
+    const styleUtils = findByProps("getStyleProp");
+
+    if (!styleUtils?.getStyleProp) {
+        console.error("[NopeRounding] getStyleProp not found");
+        return;
+    }
+
+    unpatch = after(
+        "NopeRounding",
+        styleUtils,
+        "getStyleProp",
+        (_this, args, result) => {
+            if (RADIUS_PROPS.has(args[1])) {
+                return 0;
+            }
+
+            return result;
+        },
+    );
+
+    console.log("[NopeRounding] Loaded!");
+};
+
+export const onUnload = () => {
+    unpatch?.();
+    unpatch = null;
+
+    console.log("[NopeRounding] Unloaded!");
+};
